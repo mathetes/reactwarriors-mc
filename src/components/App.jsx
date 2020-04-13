28f83a1271dae7519aad2721e985fd026e72ecd7
mathetes <mathetes@gmail.com>
2020-04-13 22:44:58 +0700

    movies list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { moviesData } from "../moviesData";
 
-function App() {
-  return <div>Hello ReactWarriors!</div>;
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: moviesData
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        { this.state.movies.map(function(movie) {
+         return <p>{movie.title}</p>; 
+      })}
+      </div>
+    );
+  }
 }
+
+
+// function App() {
+//   return <div>{ moviesData[0].title }</div>;
+// }
 
 export default App;
